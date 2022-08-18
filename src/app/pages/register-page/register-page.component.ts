7450d3fe4a2273002f5508e21ad8105c39412c10
Trieutien2001vn <19130230@st.hcmuaf.edu.vn>
2022-08-18 11:11:48 +0700

    up register page2

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.scss'],
 })
 export class RegisterPageComponent implements OnInit {
+  hide: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  registerForm: FormGroup = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(6)]],
+    passWord: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  onregister() {
+    if (!this.registerForm.valid) {
+      return;
+    }
+    console.log(this.registerForm.value);
   }
-
 }
+
