eee827fb61964719bac7d95d9f930f2fb9ec0ced
Tungnguyen038 <19130260@st.hcmuaf.edu.vn>
2022-08-19 11:43:23 +0700

    css lg c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { ChatPageComponent } from './pages/chat-page/chat-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Course1Component } from './course1/course1.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     LoginPageComponent,
     RegisterPageComponent,
     ChatPageComponent,
+    Course1Component,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
