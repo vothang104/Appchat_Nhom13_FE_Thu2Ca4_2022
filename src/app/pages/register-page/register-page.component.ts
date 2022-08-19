acefda9cb3258656537e3b5761841753ebc72b7c
Trieutien2001vn <19130230@st.hcmuaf.edu.vn>
2022-08-19 11:50:11 +0700

    upcode register page 5

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -10,7 +11,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterPageComponent implements OnInit {
   hide: boolean = false;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {}
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {}
 
   ngOnInit() {}
 
@@ -26,7 +27,9 @@ export class RegisterPageComponent implements OnInit {
     this.http.post<any>("http://localhost:3000/registerUsers", this.registerForm.value).subscribe(res=>{
       alert("Register Successfull");
       this.registerForm.reset();
-    
+      this.router.navigate(["login"]);
+    },err=>{
+      alert("Something went wrong!!");
     })
 }}
 
